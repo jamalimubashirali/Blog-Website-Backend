a1cac77c7b1031147b594445598e026d58f010dd
Mubashir Ali <21sw071@students.muet.edu.pk>
2025-03-10 22:44:42 +0500

    Refactor error handling middleware to handle headers already sent

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,4 +1,8 @@
 export const errorHandler = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+
   if (err.name === "CastError") {
     return res.status(400).json({
       success: false,
